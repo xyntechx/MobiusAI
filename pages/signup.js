b09fbcfddb0f658a5b364e1df33c4b8e59b79538
xyntechx <xyntechx@gmail.com>
2022-07-23 12:35:54 +0800

    Complete UI of /signup

---
+++ pages/signup.js
@@ -0,0 +1,102 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+
+export default function Signup() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState(""); // must be unique
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    const backerSignUp = () => {
+        // console.log(firstName, lastName, username, password1, password2);
+        // console.log("password verified:", password1 === password2);
+        window.location.href = "/backer";
+    };
+
+    const researcherSignUp = () => {
+        // console.log(firstName, lastName, username, password1, password2);
+        // console.log("password verified:", password1 === password2);
+        window.location.href = "/researcher";
+    };
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>MobiusAI</title>
+                <meta name="description" content="For a dynamic AI ecosystem" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>Sign Up</h1>
+                <br />
+                <div className={styles.form}>
+                    <div className={styles.formElement}>
+                        <p className={styles.formLabel}>First Name</p>
+                        <input
+                            type="text"
+                            className={styles.formInput}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.formElement}>
+                        <p className={styles.formLabel}>Last Name</p>
+                        <input
+                            type="text"
+                            className={styles.formInput}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.formElement}>
+                        <p className={styles.formLabel}>Username</p>
+                        <input
+                            type="text"
+                            className={styles.formInput}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.formElement}>
+                        <p className={styles.formLabel}>Password</p>
+                        <input
+                            type="password"
+                            className={styles.formInput}
+                            onChange={(e) => setPassword1(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.formElement}>
+                        <p className={styles.formLabel}>Confirm Password</p>
+                        <input
+                            type="password"
+                            className={styles.formInput}
+                            onChange={(e) => setPassword2(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.buttonDiv}>
+                        <button
+                            onClick={() => backerSignUp()}
+                            className={styles.buttonBlue}
+                        >
+                            Sign Up as Backer
+                        </button>
+                        <button
+                            onClick={() => researcherSignUp()}
+                            className={styles.buttonBlue}
+                        >
+                            Sign Up as Researcher
+                        </button>
+                    </div>
+                    <sub className={styles.guide}>
+                        Already have an account?{" "}
+                        <Link href="/login">
+                            <a className={styles.linkUnderlined}>Log in</a>
+                        </Link>{" "}
+                        instead!
+                    </sub>
+                </div>
+            </main>
+        </div>
+    );
+}
