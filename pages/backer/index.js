140d272c0b47225a4a0daee1a6651ff3f4a7f51f
xyntechx <xyntechx@gmail.com>
2022-07-24 20:54:31 +0800

    API integration

--- pages/backer/index.js
+++ pages/backer/index.js
@@ -3,12 +3,44 @@ import styles from "../../styles/User.module.css";
 import Topnav from "../../components/topnav/topnav";
 import Card from "../../components/card/card";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Backer() {
     const [usertype, setUsertype] = useState("");
+    const [campaigns, setCampaigns] = useState([]);
 
     useEffect(() => {
         setUsertype(localStorage.getItem("usertype"));
+
+        axios
+            .get(
+                "https://mztlsgg2zanq5pmemkg537k4p40dxurf.lambda-url.ap-southeast-1.on.aws/"
+            )
+            .then((res) => {
+                const temp_campaigns = [];
+                for (let i = 0; i < res.data.length; i++) {
+                    const data = res.data[i];
+
+                    const name = data.name;
+                    const raised = data.raised + data.backer_amt;
+                    const goal = data.goal;
+                    const progress =
+                        Math.round((raised / goal) * 100).toString() + "%";
+                    const imageSrc =
+                        "/placeholder" +
+                        Math.floor(Math.random() * 3 + 1) +
+                        ".png";
+
+                    temp_campaigns.push({
+                        name,
+                        raised,
+                        goal,
+                        progress,
+                        imageSrc,
+                    });
+                }
+                setCampaigns(temp_campaigns);
+            });
     }, []);
 
     return (
@@ -25,18 +57,16 @@ export default function Backer() {
                 <main className={styles.main}>
                     <h1 className={styles.title}>Active Campaigns</h1>
                     <div className={styles.container}>
-                        <Card
-                            name={"Some Random Model"}
-                            imageSrc={"/placeholder1.png"}
-                        />
-                        <Card
-                            name={"Some Random Model 2"}
-                            imageSrc={"/placeholder2.png"}
-                        />
-                        <Card
-                            name={"Some Random Model 3"}
-                            imageSrc={"/placeholder3.png"}
-                        />
+                        {campaigns.map((campaign, i) => (
+                            <Card
+                                key={i}
+                                name={campaign.name}
+                                raised={campaign.raised}
+                                goal={campaign.goal}
+                                progress={campaign.progress}
+                                imageSrc={campaign.imageSrc}
+                            />
+                        ))}
                     </div>
                 </main>
             ) : (
--- pages/researcher/index.js
+++ pages/researcher/index.js
@@ -5,12 +5,44 @@ import Topnav from "../../components/topnav/topnav";
 import Card from "../../components/card/card";
 import NewCard from "../../components/card/newcard";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Researcher() {
     const [usertype, setUsertype] = useState("");
+    const [campaigns, setCampaigns] = useState([]);
 
     useEffect(() => {
         setUsertype(localStorage.getItem("usertype"));
+
+        axios
+            .get(
+                "https://mztlsgg2zanq5pmemkg537k4p40dxurf.lambda-url.ap-southeast-1.on.aws/"
+            )
+            .then((res) => {
+                const temp_campaigns = [];
+                for (let i = 0; i < res.data.length; i++) {
+                    const data = res.data[i];
+
+                    const name = data.name;
+                    const raised = data.raised + data.backer_amt;
+                    const goal = data.goal;
+                    const progress =
+                        Math.round((raised / goal) * 100).toString() + "%";
+                    const imageSrc =
+                        "/placeholder" +
+                        Math.floor(Math.random() * 3 + 1) +
+                        ".png";
+
+                    temp_campaigns.push({
+                        name,
+                        raised,
+                        goal,
+                        progress,
+                        imageSrc,
+                    });
+                }
+                setCampaigns(temp_campaigns);
+            });
     }, []);
 
     return (
@@ -34,18 +66,16 @@ export default function Researcher() {
                         </div>
                     </div>
                     <div className={styles.container}>
-                        <Card
-                            name={"Some Random Model"}
-                            imageSrc={"/placeholder1.png"}
-                        />
-                        <Card
-                            name={"Some Random Model 2"}
-                            imageSrc={"/placeholder2.png"}
-                        />
-                        <Card
-                            name={"Some Random Model 3"}
-                            imageSrc={"/placeholder3.png"}
-                        />
+                        {campaigns.map((campaign, i) => (
+                            <Card
+                                key={i}
+                                name={campaign.name}
+                                raised={campaign.raised}
+                                goal={campaign.goal}
+                                progress={campaign.progress}
+                                imageSrc={campaign.imageSrc}
+                            />
+                        ))}
                         <NewCard />
                     </div>
                 </main>
