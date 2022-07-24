d22303ef56a3700f8dcbeae89a0a0b56e768b8e2
xyntechx <xyntechx@gmail.com>
2022-07-24 20:25:25 +0800

    Link to homepage

--- components/topnav/topnav.js
+++ components/topnav/topnav.js
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import styles from "../topnav/Topnav.module.css";
+import { useState, useEffect } from "react";
 
 export default function Topnav() {
+    const [usertype, setUsertype] = useState("");
+
+    useEffect(() => {
+        setUsertype(localStorage.getItem("usertype"));
+    }, []);
+
     const logout = () => {
         localStorage.clear();
         window.location.href = "/";
@@ -9,9 +16,19 @@ export default function Topnav() {
 
     return (
         <header className={styles.nav}>
-            <h1 className={styles.name}>
-                Mobius<span className={styles.blue}>AI</span>
-            </h1>
+            {usertype === "researcher" ? (
+                <Link href="/researcher">
+                    <h1 className={styles.name}>
+                        Mobius<span className={styles.blue}>AI</span>
+                    </h1>
+                </Link>
+            ) : (
+                <Link href="/backer">
+                    <h1 className={styles.name}>
+                        Mobius<span className={styles.blue}>AI</span>
+                    </h1>
+                </Link>
+            )}
             <div className={styles.navDiv}>
                 <button className={styles.button} onClick={() => logout()}>
                     Log Out
