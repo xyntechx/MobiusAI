ae8632e827c08a2bfa4a4de0517a587d54cac7de
xyntechx <xyntechx@gmail.com>
2022-07-24 21:18:59 +0800

    Clean up code

--- components/card/newcard.js
+++ components/card/newcard.js
@@ -5,20 +5,6 @@ import { useState, useEffect } from "react";
 
 export default function NewCard() {
     return (
-        // <Link href={"/campaign/" + name}>
-        //     <div className={styles.card}>
-        //         <div className={styles.image}>
-        //             <Image
-        //                 src={imageSrc}
-        //                 layout="fill"
-        //                 alt="Model placeholder image"
-        //             />
-        //         </div>
-        //         <p className={styles.name}>{name}</p>
-        //         {/* TODO: Progress bar */}
-        //     </div>
-        // </Link>
-
         <Link href="/researcher/new">
             <a className={styles.newcard}>
                 <p>Launch New Campaign</p>
